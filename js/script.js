1dfe47d89abd413e987975e16cba06300e4130d9
Camila Inocencio <camilamarq3004@gmail.com>
2023-02-23 16:29:03 -0300

    Jogo finalizado

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ var positionY;
 var velocity;
 var plane;
 var time;
+
 const lplane = document.querySelector(".plane");
 const star = document.querySelector(".star");
 const starI = document.querySelector(".starI");
@@ -13,7 +14,10 @@ const score = document.querySelector(".score");
 const background = document.querySelector(".background");
 const little_screen = document.querySelector(".little_screen");
 const play = document.querySelector(".play");
+const ktop = document.querySelector(".top");
+const kbottom = document.querySelector(".bottom");
 
+// Função da tela inicial
 function game(){
     lplane.style.display = "none";
     star.style.display = "none";
@@ -21,8 +25,11 @@ function game(){
     score.style.display = "none";
     stars.style.display = "none";
     background.style.display = "none";
+    ktop.style.display = "none";
+    kbottom.style.display = "none";
 }
 
+// Função que inicia o jogo
 function start(){
     play.style.display = "none";
     background.style.display = "";
@@ -39,19 +46,22 @@ function start(){
     positionY = 75;
     velocity = 10;
     plane = document.getElementById("plane");
-    time = setInterval(enter,20);
+    time = setInterval(move,20);
     document.addEventListener("keydown", keyd);
     document.addEventListener("keyup", keyu);
 }
 
+// Funções de movimentação com as teclas
 function keyd (event){
     let key = event.key;
 
     if (key === "ArrowUp") {
         directionY = -1;
+        ktop.style.display = "";
     }  
     else if (key === "ArrowDown") {
         directionY = 1;
+        kbottom.style.display = "";
     }
 }
 
@@ -60,16 +70,19 @@ function keyu (event) {
 
     if (key === "ArrowUp") {
         directionY = 0;
+        ktop.style.display = "none";
     } 
     else if (key === "ArrowDown") {
         directionY = 0;
+        kbottom.style.display = "none";
     }
 }
 
 var points = 0;
 const loop = setInterval(verify,10);
 
-function enter(){
+// Função de movimentação do aviãozinho
+function move(){
     positionY += directionY*velocity;
 
     plane.style.top = positionY+"px";
@@ -78,6 +91,7 @@ function enter(){
     starI.style.display = "";
 }
 
+// Função que verifica a posição do aviãozinho
 function verify(){
     const starL = star.offsetLeft;
     const starT = star.offsetTop;
@@ -89,7 +103,7 @@ function verify(){
         star.style.display = "none";
         score.innerHTML = points;
     }
-    if(planeT >= 140 && planeT <= 185 && starIL <= 100 && starIL >= 20){
+    if(planeT >= 145 && planeT <= 185 && starIL <= 100 && starIL >= 20){
         points += 5;
         starI.style.display = "none";
         score.innerHTML = points;
