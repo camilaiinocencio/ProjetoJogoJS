f9cb0230ab1eaf1249386ebc47d67af57893925b
Camila Inocencio <camilamarq3004@gmail.com>
2023-02-22 23:25:09 -0300

    aperfeicoamento do sistema de pontuacao e layout dinamico

--- js/script.js
+++ js/script.js
@@ -5,12 +5,38 @@ var positionY;
 var velocity;
 var plane;
 var time;
+const lplane = document.querySelector(".plane");
+const star = document.querySelector(".star");
+const starI = document.querySelector(".starI");
+const stars = document.querySelector(".stars");
+const score = document.querySelector(".score");
+const background = document.querySelector(".background");
+const little_screen = document.querySelector(".little_screen");
+const play = document.querySelector(".play");
+
+function game(){
+    lplane.style.display = "none";
+    star.style.display = "none";
+    starI.style.display = "none";
+    score.style.display = "none";
+    stars.style.display = "none";
+    background.style.display = "none";
+}
 
 function start(){
+    play.style.display = "none";
+    background.style.display = "";
+    little_screen.style.display = "none";
+    lplane.style.display = "";
+    star.style.display = "";
+    starI.style.display = "";
+    score.style.display = "";
+    stars.style.display = "";
+
     directionX = 0;
     directionY = 0;
     positionX = 0;
-    positionY = 50;
+    positionY = 75;
     velocity = 10;
     plane = document.getElementById("plane");
     time = setInterval(enter,20);
@@ -40,38 +66,34 @@ function keyu (event) {
     }
 }
 
+var points = 0;
+const loop = setInterval(verify,10);
+
 function enter(){
     positionY += directionY*velocity;
 
     plane.style.top = positionY+"px";
-}
 
-var points = 0;
-const loop = setInterval(verify,100);
-const lplane = document.querySelector(".plane");
-const star = document.querySelector(".star");
-const starI = document.querySelector(".starI");
+    star.style.display = "";
+    starI.style.display = "";
+}
 
 function verify(){
     const starL = star.offsetLeft;
     const starT = star.offsetTop;
     const starIL = starI.offsetLeft;
-    const starIT = starI.offsetTop;
-    const planeL = lplane.offsetLeft;
     const planeT = lplane.offsetTop;
-    var difL = starL - planeL;
-    var difT = starT - planeT;
-    var IdifL = starIL - planeL;
-    var IdifT = starIT - planeT;
     
-    if(difL <= 130 && difL >= 115 && difT <= 75 && difT >= 10){
+    if(planeT >= -30 && planeT <= 30 && starL <= 100 && starL >= 20){
         points += 10;
-        console.log(points);
+        star.style.display = "none";
+        score.innerHTML = points;
     }
-    if(IdifL <= 130 && IdifL >= 120 && IdifT <= 80 && IdifT >= 20){
+    if(planeT >= 140 && planeT <= 185 && starIL <= 100 && starIL >= 20){
         points += 5;
-        console.log(points);
+        starI.style.display = "none";
+        score.innerHTML = points;
     }
 }
 
-window.addEventListener("load", start);
+window.addEventListener("load", game);
